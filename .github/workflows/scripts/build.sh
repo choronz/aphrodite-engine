0d1aab24c1322bdf643af993da83aeffd9ebc28a
Robin <choronz@users.noreply.github.com>
2024-09-27 03:28:33 +0800

    Update build.sh

--- .github/workflows/scripts/build.sh
+++ .github/workflows/scripts/build.sh
@@ -14,8 +14,7 @@ $python_executable -m pip install -r requirements-cuda.txt
 
 # Limit the number of parallel jobs to avoid OOM
 export MAX_JOBS=2
-
 export TORCH_CUDA_ARCH_LIST="8.9"
 
 # Build
-$python_executable setup.py bdist_wheel --dist-dir=dist --py-limited-api=dist
+$python_executable setup.py bdist_wheel --dist-dir=dist --py-limited-api=cp312
