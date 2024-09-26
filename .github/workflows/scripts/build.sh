8bfe25f60e5089252da77aa2276ae3a773981a20
Robin <choronz@users.noreply.github.com>
2024-09-27 01:18:03 +0800

    Update build.sh

--- .github/workflows/scripts/build.sh
+++ .github/workflows/scripts/build.sh
@@ -17,4 +17,5 @@ export MAX_JOBS=2
 export TORCH_CUDA_ARCH_LIST="8.9"
 
 # Build
-$python_executable setup.py bdist_wheel --dist-dir=dist --py-limited-api=cp$1
+#echo $1 | tr '.' '\0'
+$python_executable setup.py bdist_wheel --dist-dir=dist --py-limited-api=cp${$1/'.'/'\0'}
